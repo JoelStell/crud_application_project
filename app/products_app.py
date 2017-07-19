5a5c2b6e05039625f5f64ddea41fe711ce4107c2
JoelStell <jjs757@stern.nyu.edu>
2017-07-19 18:42:24 -0400

    first two steps

--- app/products_app.py
+++ app/products_app.py
@@ -1,1 +1,20 @@
-    print("HELLO")
+print("----------------------------------")
+print("PRODUCTS APPLICATION")
+print("----------------------------------")
+print("Welcome joelstell" + "\n")
+
+menu = """
+    Hi.
+    Welcome to the products app.
+
+    There are 100 products.
+
+    Available operations: 'List', 'Show', 'Create', 'Update', 'Destroy'
+
+    Please choose an operation
+    """
+
+
+chosen_operation = input(menu)
+
+print(chosen_operation)
