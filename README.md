796ddcf38d0ccf732ba3732c083bb8c6e58e3436
JoelStell <jjs757@stern.nyu.edu>
2017-07-24 13:52:41 -0400

    Finished Checkpoint 3

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Name of Your App
 
-Some description or other info about what the app does.
+This program is a CRUD application which will help store managers keep track of their inventory of products. CRUD is an acronym for "Create", "Read", "Update", and "Destroy". These operations represent the primary actions performed on database resources (i.e. records) within an information system. The "Read" operation comprises two operations: the "List" operation for reading all resources, as well as the "Show" operation for reading a single resource.
 
 ## Installation
 
 Download the source code:
 
 ```shell
-git clone remote_clone_address_of_your_repo_here
-cd some/path/to/repo/
+git clone https://github.com/JoelStell/crud_application_project
+cd Desktop/crud_application_project
 ```
 
 Finally, download the [example `products.csv` file](https://raw.githubusercontent.com/prof-rossetti/nyu-info-2335-70-201706/master/projects/crud-app/products.csv) and save it as `data/products.csv`.
@@ -16,5 +16,5 @@ Finally, download the [example `products.csv` file](https://raw.githubuserconten
 ## Usage
 
 ```shell
-python app/products_app.py
+crud_application_project/app/products_app.py
 ```
--- app/products_app.py
+++ app/products_app.py
@@ -14,6 +14,34 @@ with open(csv_file_path, "r") as csv_file:
 
 #products = sorted(products, key=operator.itemgetter("id"))
 
+def lookup_product(product_id):
+    matching_products = [p for p in products if p["id"] == product_id]
+    return matching_products[0]
+
+def handle_index_error():
+    print("OOPS. There are no products matching the given identifier. Try listing products to see which ones exist.")
+
+def user_inputs_product_id():
+    product_id = input("OK. Please specify the product's identifier: ")
+    return product_id
+
+def prompt_user_for_product_info():
+    print("OK. Please specify the product's information...")
+
+def handle_index_error():
+    print("OOPS. There are no products matching the given identifier. Try listing products to see which ones exist.")
+
+def user_inputtable_headers():
+    return [header for header in headers if header != "id"]
+
+def auto_increment_id(products):
+    product_ids = list(map(map_id, products))
+    if len(product_ids) == 0: next_id = 1
+    else: next_id = max(product_ids) + 1
+    return next_id
+
+headers = ["id", "name", "aisle", "department", "price"]
+
 menu = """
     Hi.
 
@@ -31,8 +59,9 @@ chosen_operation = input(menu)
 chosen_operation = chosen_operation.title()
 
 def list_products():
+    print("THERE ARE", len(products), "PRODUCTS:")
     for product in products:
-        print(product["id"], product["name"])
+        print("  +", product) #product["id"], "product["name"], product["aisle"], product["department"], product["price"])
 
 def show_product():
     while True:
@@ -59,8 +88,47 @@ def create_product():
     print("NEW PRODUCT IS", new_product)
     products.append(new_product)
 
-def update_product():
-    print("UPDATING A PRODUCT")
+# def update_product():
+#     print("UPDATING A PRODUCT")
+#     while True:
+#         product_id = input("Please input a product identifier, or 'DONE' if there are no more items: ")
+#         if product_id == "DONE":
+#             break
+#         else:
+#             products[:] = [d for d in products if d.get('id') != product_id]
+#             product_name = input("name is:")
+#             product_aisle = input("aisle is:")
+#             product_department = input("department is:")
+#             product_price = input("price is:")
+#             new_product = {
+#                 "id": int(product_id),
+#                 "name": product_name,
+#                 "aisle": product_aisle,
+#                 "department": product_department,
+#                 "price": product_price
+#                 }
+#             #old_product = []
+#             #old_product[:] = [d for d in products if d.get('id') == product_id]
+#             #products[:] = [d for d in products if d.get('id') != product_id]
+#             products.append(new_product)
+#             new_list = sorted(products, key=operator.itemgetter("id"))
+#             print(new_list)
+# #products = sorted(products, key=operator.itemgetter("id"))
+#
+#             #products = sorted(products, key=operator.itemgetter("id"))
+
+def update_product(products):               #help from Professor Rossetti
+    product_id = user_inputs_product_id()
+    try:
+        product = lookup_product(product_id)
+        prompt_user_for_product_info()
+        for header in user_inputtable_headers():
+            product[header] = input("    Change {0} from '{1}' to: ".format(header, product[header]))
+        print("UPDATING A PRODUCT HERE!")
+        print(product)
+        return product
+    except IndexError as e:
+        handle_index_error()
 
 def destroy_product():
     print("DESTROYING A PRODUCT")
@@ -81,7 +149,7 @@ def destroy_product():
 if chosen_operation == "List": list_products()
 elif chosen_operation == "Show": show_product()
 elif chosen_operation == "Create": create_product()
-elif chosen_operation == "Update": update_product()
+elif chosen_operation == "Update": update_product(products)
 elif chosen_operation == "Destroy": destroy_product()
 else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
 
@@ -89,7 +157,7 @@ else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
 # OVERWRITING INVENTORY CSV FILE
 
 with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=["id","name","aisle","department","price"])
+    writer = csv.DictWriter(csv_file, fieldnames = headers)
     writer.writeheader() # uses fieldnames set above
     for product in products:
         writer.writerow(product)
